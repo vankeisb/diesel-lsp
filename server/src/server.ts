7120b13f255cd992c65b09d26385aefa06129eb6
Rémi Van Keisbelck <remi@rvkb.com>
2022-11-25 16:00:44 +0100

    cleanup

--- server/src/server.ts
+++ server/src/server.ts
@@ -287,24 +287,6 @@ connection.onCompletion(
 				data: p,
 			};
 		}); 
-
-
-
-		// // The pass parameter contains the position of the text document in
-		// // which code complete got requested. For the example we ignore this
-		// // info and always provide the same completion items.
-		// return [
-		// 	{
-		// 		label: 'TypeScript',
-		// 		kind: CompletionItemKind.Text,
-		// 		data: 1
-		// 	},
-		// 	{
-		// 		label: 'JavaScript',
-		// 		kind: CompletionItemKind.Text,
-		// 		data: 2
-		// 	}
-		// ];
 	}
 );
 
